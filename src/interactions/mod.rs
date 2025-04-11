23481bc38fe886085670dc84681127a406bb0b81
Brandon White <bwhite@mdlogic.com>
2025-04-11 00:06:32 -0400

    [Feature] - Module declaration file fix (the gjk module was auto-imported below test modules causing comp warnings)

--- src/interactions/mod.rs
+++ src/interactions/mod.rs
@@ -6,6 +6,7 @@ pub use interactions::*;
 pub use interactions_2d::*;
 pub use interactions_3d::*;
 pub mod shape_collisions_3d;
+pub mod gjk_collision_3d;
 
 #[cfg(test)]
 mod interactions_tests;
@@ -15,4 +16,3 @@ mod interactions_2d_tests;
 mod interactions_3d_tests;
 #[cfg(test)]
 mod shape_collisions_3d_tests;
-mod gjk_collision_3d;
